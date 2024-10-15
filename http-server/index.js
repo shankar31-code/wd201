ffe01026c8616d3a6c1acce0518005b271f53004
shankar31-code <shankarpullur5@gmail.com>
2024-10-15 15:08:36 +0530

    Fix registration.html syntax and update todo-cli submodule

---
+++ http-server/index.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-undef */
+const http = require("http");
+const fs = require("fs");
+const args = require("minimist")(process.argv);
+const port = args.port || 3000;
+let homecontent = "";
+let projectcontent = "";
+let registercontent = "";
+fs.readFile("./home.html", (err, home) => {
+  if (err) {
+    throw error;
+  }
+  homecontent = home;
+});
+fs.readFile("./project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectcontent = project;
+});
+fs.readFile("./registration.html", (err, register) => {
+  if (err) {
+    throw err;
+  }
+  registercontent = register;
+});
+const server = http.createServer((req, res) => {
+  let url = req.url;
+  res.writeHeader(200, { "content-Type": "text/html" });
+  switch (url) {
+    case "/project":
+      res.write(projectcontent);
+      res.end();
+      break;
+    case "/registration.html":
+      res.write(registercontent);
+      res.end();
+      break;
+    default:
+      res.write(homecontent);
+      res.end();
+      break;
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
